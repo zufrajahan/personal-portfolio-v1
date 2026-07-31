176e43c7f7a6722c4cc18f116c0947e5f50d50a6
zufrajahan <zufra.jahan500@gmail.com>
2026-07-31 14:56:55 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+alert('welcome to my portfolio')
+
+function changemode() {
+    const element = document.getElementById('buttonOne')
+    if (element) {
+        document.body.style.backgroundColor = 'rgb(45, 45, 60)'
+        document.body.style.color = 'white'
+        
+    }
+}
+
